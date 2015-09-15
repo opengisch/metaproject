f53785a5129c62d29364d1980043c89810c5f64d
Matthias Kuhn <matthias@opengis.ch>
2015-09-15 07:53:45 +0200

    Code formatting

--- tools/pg_to_metaproject.py
+++ tools/pg_to_metaproject.py
@@ -1,37 +1,49 @@
 import psycopg2
+import psycopg2.extras
 import json
 from collections import OrderedDict
 
-def add_field(fields_dict, fieldname, nullable, data_type):
-    if fieldname not in fields_dict:
-        fields_dict[fieldname]=dict()
-    fields_dict[fieldname]['nullable']=True if nullable=='YES' else False
-    fields_dict[fieldname]['data_type']=data_type
+def add_field(fields_dict, field_info):
+    fieldname = field_info['column_name']
+    nullable = field_info['is_nullable']
+    data_type = field_info['udt_name']
+
+    if fieldname in fields_dict:
+        field = fields_dict[fieldname]
+    else:
+        field = dict()
+
+    field['nullable'] = True if nullable == 'YES' else False
+    field['data_type'] = data_type
+
+    if data_type == 'numeric':
+        field['precision'] = field_info['numeric_scale']
+
+    if data_type == 'varchar':
+        field['length'] = field_info['character_maximum_length']
+    fields_dict[fieldname] = field
 
 # Connect to the database
 conn = psycopg2.connect("service=pg_qgep")
 
-cur=conn.cursor()
-
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # Select all fields to initialize the table metainformation
 cur.execute("SELECT * FROM information_schema.columns WHERE table_schema='qgep'")
-colnames = [desc[0] for desc in cur.description]
+
 pg_fields = cur.fetchall()
 
-tables=dict()
+tables = dict()
 
 for f in pg_fields:
-    tablename=f[colnames.index('table_name')]
-    fieldname=f[colnames.index('column_name')]
-    nullable=f[colnames.index('is_nullable')]
-    data_type=f[colnames.index('udt_name')]
+    tablename = f['table_name']
+    fieldname = f['column_name']
 
     if tablename not in tables:
-        tables[tablename]=dict()
+        tables[tablename] = dict()
         tables[tablename]['fields'] = OrderedDict()
 
-    add_field(tables[tablename]['fields'], fieldname, nullable, data_type)
+    add_field(tables[tablename]['fields'], f)
 
 # Find primary keys
 cur.execute("""
@@ -50,11 +62,11 @@ colnames = [desc[0] for desc in cur.description]
 pg_pks = cur.fetchall()
 
 for pk in pg_pks:
-    table_name=pk[colnames.index('table_name')]
-    column_name=pk[colnames.index('column_name')]
+    table_name = pk[colnames.index('table_name')]
+    column_name = pk[colnames.index('column_name')]
 
     try:
-        tables[table_name]['fields'][column_name]['primary_key']=True
+        tables[table_name]['fields'][column_name]['primary_key'] = True
     except KeyError:
         pass
 
@@ -77,23 +89,25 @@ colnames = [desc[0] for desc in cur.description]
 pg_constraints = cur.fetchall()
 
 for c in pg_constraints:
-    table_name=c[colnames.index('table_name')]
-    column_name=c[colnames.index('column_name')]
-    foreign_table_name=c[colnames.index('foreign_table_name')]
-    foreign_column_name=c[colnames.index('foreign_column_name')]
+    table_name = c[colnames.index('table_name')]
+    column_name = c[colnames.index('column_name')]
+    foreign_table_name = c[colnames.index('foreign_table_name')]
+    foreign_column_name = c[colnames.index('foreign_column_name')]
 
-    fk=dict()
-    fk['table']=foreign_table_name
-    fk['column']=foreign_column_name
+    fk = dict()
+    fk['table'] = foreign_table_name
+    fk['column'] = foreign_column_name
 
     if 'primary_key' in tables[table_name]['fields'][column_name]:
         tables[table_name]['inherits'] = foreign_table_name
 
-    tables[table_name]['fields'][column_name]['references']=fk
+    tables[table_name]['fields'][column_name]['references'] = fk
+
+
 
 # Assemble the metaproject information
-mp=dict()
+mp = dict()
 mp['tables'] = tables
 
-with open('qgep_base.mp', 'w') as f:
+with open('../data/qgep_base.mp', 'w') as f:
     f.write(json.dumps(mp, indent=2))
